ddb3f8f53bf83fde6e468f344f318fe97223ce56
Wei Ding <w-ding@unext.jp>
2020-06-09 13:18:12 +0900

    make email check regexp simpler, and enlarge domain length check from {2,3} to {2,} that can fit .asia, .tokyo and so on.

--- KafkaCenter-Frontend/src/pages/Setting/User/components/User/index.js
+++ KafkaCenter-Frontend/src/pages/Setting/User/components/User/index.js
@@ -150,7 +150,7 @@ export default class User extends Component {
   }
 
    validateEmail = (rule, value, callback) => {
-     const reg = /^([a-zA-Z0-9\-]+[_|\\_|\\.]?)*[a-zA-Z0-9\-]+@([a-zA-Z0-9]+[_|\\_|\\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+     const reg = /^[A-Za-z0-9\\_\\-\\.]+@[A-Za-z0-9\\_\\-\\.]+\.[A-Za-z]{2,}$/;
      if (!reg.test(value)) {
        return callback(new Error('email address is invaild,please check!'));
      }
